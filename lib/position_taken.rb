99ae5c2573252ecc466e86248950ca5f366a2cd0
Theresa Morelli <theresa.l.morelli@gmail.com>
2018-05-09 20:01:02 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,7 +2,7 @@
 def position_taken?(board, index)
   if board[index] == " " || "" || nil
     false
-  elsif board[index] == "X" || "O"
+  elsif board[index] == "X" || board[index] == "O"
     true
   end
 end
